a84e7672b5635e6e21f1e292ebd9b807a277b54f
vipulchaube <vipul@milestogo.tv>
2021-09-28 12:09:16 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -68,5 +68,22 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>ORDER VALUE<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void order_value_should_get_cumulative_total_when_collection_of_items_selected(){
+        restaurantCreate();
+        order = restaurant.getMenu();
+        assertEquals(568,restaurant.getOrderValue(order));
+    }
 
+    @Test
+    public void order_value_should_reduce_cumulative_total_when_an_item_removed(){
+        restaurantCreate();
+        order = restaurant.getMenu();
+        int total = restaurant.getOrderValue(order);
+        int afterTotal = order.get(1).getPrice();
+        order.remove(1);
+        assertEquals(total-afterTotal,restaurant.getOrderValue(order));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<<<ORDER VALUE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 }
